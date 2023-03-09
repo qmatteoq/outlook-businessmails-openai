0f995f5f5baa1504cf1bedea447e088bdb3380aa
Matteo Pagani <matteo.pagani@microsoft.com>
2023-03-09 15:45:35 +0100

    removed api key

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -67,7 +67,7 @@ export default class App extends React.Component<AppProps, AppState> {
     // eslint-disable-next-line no-undef
     var current = this;
     const configuration = new Configuration({
-      apiKey: "***REMOVED***",
+      apiKey: "your-api-key",
     });
     const openai = new OpenAIApi(configuration);
     current.setState({ isLoading: true });
